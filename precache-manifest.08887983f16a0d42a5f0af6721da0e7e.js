0d4c94ffde0f8b0e08bf8c60a4f31602f65a4b48
Gifari <gifari.root@gmail.com>
2023-07-27 20:49:54 -0400

    Updated resume on 07272023

---
+++ precache-manifest.08887983f16a0d42a5f0af6721da0e7e.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037c02d90a4a8323f4553c39937b53a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5d542f47e9bfff0eef1",
+    "url": "/static/css/main.aec071e7.chunk.css"
+  },
+  {
+    "revision": "f67bdbec51c9584b5dca",
+    "url": "/static/js/2.10641a42.chunk.js"
+  },
+  {
+    "revision": "e5d542f47e9bfff0eef1",
+    "url": "/static/js/main.9e304d5b.chunk.js"
+  },
+  {
+    "revision": "6138eaf4a6c67cbf898e",
+    "url": "/static/js/runtime-main.0403a33d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c03f3410cc70592a95602238a5e91088",
+    "url": "/static/media/ThetaTauLogo.c03f3410.png"
+  },
+  {
+    "revision": "6fe70c7d1bc96f244a9501b498236051",
+    "url": "/static/media/army_logo.6fe70c7d.png"
+  },
+  {
+    "revision": "5f6147fad0dab40a5f449a9c915b4fd2",
+    "url": "/static/media/business_newspaper.5f6147fa.jpg"
+  },
+  {
+    "revision": "989f59139e262247fa4672c6073ec3c4",
+    "url": "/static/media/cool_computers.989f5913.jpg"
+  },
+  {
+    "revision": "434d2b012c3efc3da1ef90549a50f46b",
+    "url": "/static/media/data_science.434d2b01.svg"
+  },
+  {
+    "revision": "8b6aa15afeebfeed5cef50e357cd1637",
+    "url": "/static/media/dbmanage.8b6aa15a.svg"
+  },
+  {
+    "revision": "101c2310195a6d0853632299b284e530",
+    "url": "/static/media/dream_big.101c2310.jpg"
+  },
+  {
+    "revision": "caed892b2a0b817a8e275ae29638edaf",
+    "url": "/static/media/education.caed892b.svg"
+  },
+  {
+    "revision": "d5dc9f23e943356d2c93259a56c86814",
+    "url": "/static/media/ermg_logo.d5dc9f23.png"
+  },
+  {
+    "revision": "98a77d607c1271dfdc4066e4f0095489",
+    "url": "/static/media/experience.98a77d60.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "a73310ea0ad8aebedb65b3106024427a",
+    "url": "/static/media/feelingProud.a73310ea.svg"
+  },
+  {
+    "revision": "c29286db43f093fb31867a8db3140cb1",
+    "url": "/static/media/fullstop.c29286db.svg"
+  },
+  {
+    "revision": "bdb5ae613b00d1db9b077f13fbe09520",
+    "url": "/static/media/gifari_hackerpic.bdb5ae61.svg"
+  },
+  {
+    "revision": "a8e4c9fc13c42112ecb63e1e7a442eea",
+    "url": "/static/media/gifari_hackerpic_smaller.a8e4c9fc.svg"
+  },
+  {
+    "revision": "b967900b0ca604f7f60c2554e4daaf14",
+    "url": "/static/media/gifari_logo_tr.b967900b.PNG"
+  },
+  {
+    "revision": "485664e05107eb45f63a5c912cd3755d",
+    "url": "/static/media/gifari_logo_transparent.485664e0.PNG"
+  },
+  {
+    "revision": "120bbf8c85ca4ab3c1a75e8792ec4111",
+    "url": "/static/media/gifari_logo_transparent_main.120bbf8c.PNG"
+  },
+  {
+    "revision": "b0876e6e0ed660281d7c36b559e165c8",
+    "url": "/static/media/gifari_logo_transparent_new.b0876e6e.PNG"
+  },
+  {
+    "revision": "2ac3d5b63e46cadfb98ccb21694a875e",
+    "url": "/static/media/gifari_logo_transparent_official.2ac3d5b6.PNG"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "/static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "1e0956ae82674a8b8693566785713697",
+    "url": "/static/media/hacker.1e0956ae.svg"
+  },
+  {
+    "revision": "b2db6da96781798f5c8e733685e9b1fc",
+    "url": "/static/media/hunt_logo.b2db6da9.png"
+  },
+  {
+    "revision": "7b7fa675809c25698645185d0ceb1d5a",
+    "url": "/static/media/hunt_logo_transparent.7b7fa675.png"
+  },
+  {
+    "revision": "7204baa7b93e8273a8037ea254d92be5",
+    "url": "/static/media/linkedin_trophy.7204baa7.PNG"
+  },
+  {
+    "revision": "d0e92bafdc01018593505fb1802dfe4c",
+    "url": "/static/media/linkedin_trophy_transparent.d0e92baf.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "eddc246f0ffc5d9bcf5bbefabc7f0399",
+    "url": "/static/media/missing_pages.eddc246f.jpg"
+  },
+  {
+    "revision": "12be1505392328d4f4e85dd927c72a80",
+    "url": "/static/media/missing_piece.12be1505.jpg"
+  },
+  {
+    "revision": "a82daf6813d901509548a306d60e6f3f",
+    "url": "/static/media/missing_piece2.a82daf68.jpg"
+  },
+  {
+    "revision": "f2c0886d0089082b8ced8b43c8e0337b",
+    "url": "/static/media/opportunity_loss_logo.f2c0886d.png"
+  },
+  {
+    "revision": "115fc37660f513dca0c1b920ea7c8e8b",
+    "url": "/static/media/opportunity_loss_logo_animated.115fc376.gif"
+  },
+  {
+    "revision": "9f93ff3659e81da132a63551ce94087a",
+    "url": "/static/media/pinehill_logo.9f93ff36.png"
+  },
+  {
+    "revision": "92f97c4390ee884683276eb801cd7581",
+    "url": "/static/media/purple_ermg_logo.92f97c43.png"
+  },
+  {
+    "revision": "03e3faa16840e3bb691303b8ba317592",
+    "url": "/static/media/roads 1.03e3faa1.svg"
+  },
+  {
+    "revision": "9c72744412cf1dd85f0e2ece468e9afa",
+    "url": "/static/media/roads.9c727444.svg"
+  },
+  {
+    "revision": "e2190b385b014e1e56366cb343dec610",
+    "url": "/static/media/sklearn_logo.e2190b38.png"
+  },
+  {
+    "revision": "10a41e5f270b2412e0e3cb4edf966778",
+    "url": "/static/media/softwaredev.10a41e5f.svg"
+  },
+  {
+    "revision": "d7d6a16a76c745e5b295a363bdd810c0",
+    "url": "/static/media/ub_logo.d7d6a16a.png"
+  },
+  {
+    "revision": "0312e6f116bef2facb69bc08d0b76951",
+    "url": "/static/media/wallstreet.0312e6f1.jpg"
+  }
+]);
